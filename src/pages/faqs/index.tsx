1a6b7db9f359f0d95161d74df13bbcb2e987c334
marienv <mariela.n.vallejos@gmail.com>
2023-09-04 14:59:25 -0300

    fix faqs Vercel url

--- src/pages/faqs/index.tsx
+++ src/pages/faqs/index.tsx
@@ -3,50 +3,39 @@ import { Faq } from '@/types/faqs'
 import { NextPage } from 'next'
 import styles from './Faq.module.css'
 
-// interface Props{
-//   faqs: Faq[]
-// }
+interface Props{
+  faqs: Faq[]
+}
 
 
-// const faqsPage: NextPage<Props> = ({ faqs }) => {
+const faqsPage: NextPage<Props> = ({ faqs }) => {
 
-//   return (
-//     <Layout title="Preguntas Frecuentes">
-//       <div className={styles.container}>
-//         {faqs.map((faq) => (
-//           <div key={faq.id}>
-//             <h2>{faq.question}</h2>
-//             <p>{faq.answer}</p>
-//           </div>
-//           ))}
-//       </div>
-//      </Layout>
-//   )
-// }
-
-// export const getStaticProps = async () => {
-
-//   // Modificar la url por las que nos da Vercel al hacer deploy
-//   const response = await fetch('http://localhost:3000/api/faqs')
-//   const faqs = await response.json()
-
-//   return {
-//     props: {
-//       faqs
-//     }
-//   }
-
-// }
-
-// export default faqsPage
-
-import React from 'react'
-
-
-const FaqPage2 = () => {
   return (
-    <div> FaqPage2</div>
+    <Layout title="Preguntas Frecuentes">
+      <div className={styles.container}>
+        {faqs.map((faq) => (
+          <div key={faq.id}>
+            <h2>{faq.question}</h2>
+            <p>{faq.answer}</p>
+          </div>
+          ))}
+      </div>
+     </Layout>
   )
 }
 
-export default FaqPage2
+export const getStaticProps = async () => {
+
+  // Modificar la url por las que nos da Vercel al hacer deploy
+  const response = await fetch('https://ecommerce-app-wine.vercel.app/api/faqs')
+  const faqs = await response.json()
+
+  return {
+    props: {
+      faqs
+    }
+  }
+
+}
+
+export default faqsPage
